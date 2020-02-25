dc94b20ef46ec9da789e38fa5bd7ff96a707db67
anjwoc <anjwoc@naver.com>
2020-02-26 00:21:59 +0900

    fix: syntax error

--- lambda/index.js
+++ lambda/index.js
@@ -4,36 +4,34 @@ const Sharp = require('sharp');
 const s3 = new AWS.S3({ region: 'ap-northeast-2' });
 
 exports.handler = async (event, context, callback) => {
-  // event 객체에 Bucket들 이름이 들어있다.
-  const Bucket = event.Records[0].s3.Bucket.name;
-  // key는 파일 이름의 확장자를 제외한 앞부분
+  const Bucket = event.Records[0].s3.bucket.name;
   const Key = event.Records[0].s3.object.key;
-  const filename = Key.split('/')[Key.split('/').length-1];
-  const ext = Key.split('.')[Key.split('.').length-1];
+  const filename = Key.split('/')[Key.split('/').length - 1];
+  const ext = Key.split('.')[Key.split('.').length - 1];
   console.log(Key, filename, ext);
-  const requireFormat = ext === 'jpg' ? 'jpeg' : ext; // sharp에서는 jpg 대신에 jpeg를 사용한다.
-  try{
-    const s3Object = await S3.getObject({ // S3에서 이미지를 받아온다.
+  const requiredFormat = ext === 'jpg' ? 'jpeg' : ext; // sharp에서는 jpg 대신 jpeg사용합니다
+  try {
+    const s3Object = await S3.getObject({ // S3에서 이미지를 받아 옵니다.
       Bucket,
       Key,
     }).promise();
     console.log('original', s3Object.Body.length);
-    const resizeImage = await Sharp(s3Object.Body)
+    const resizedImage = await Sharp(s3Object.Body)
       .resize(800, 800, {
         fit: 'inside',
       })
-      .toFormat(requireFormat)
+      .toFormat(requiredFormat)
       .toBuffer();
-    console.log('resize', resizeImage.length);
+    console.log('resize', resizedImage.length);
     await S3.putObject({
-      Body: resizeImage,
+      Body: resizedImage,
       Bucket,
-      Key: `thumb/${filename}`
+      Key: `thumb/${filename}`,
     }).promise();
+    console.log('put');
     return callback(null, `thumb/${filename}`);
-
-  }catch(err){
-    console.error(err);
-    return callback(err);
+  } catch (error) {
+    console.error(error);
+    return callback(error);
   }
 };

8ade93ede0ca26a18c72804c41e1f80b469abbb9
anjwoc <anjwoc@naver.com>
2020-03-22 18:43:37 +0900

    Add: server.js nuxt setting

--- front/server.js
+++ front/server.js
@@ -1,20 +1,37 @@
 'use strict';
+const { Nuxt, Builder } = require('nuxt');
+const app = require('./app.js');
+const isProd = (process.env.NODE_ENV === 'production');
+const config = require('./nuxt.config.js');
+config.dev = !isProd;
+const nuxt = new Nuxt(config);
 
-var app = require('./app.js');
+app.use(nuxt.render);
 
-require('greenlock-express')
-    .init({
-        packageRoot: __dirname,
+if (config.dev){
+  new Builder(nuxt).build()
+    .then(listen);
+} else {
+  listen();
+}
 
-        // contact for security and critical bug notices
-        maintainerEmail: "anjwoc@gmail.com",
+function listen() {
+  require('greenlock-express')
+  .init({
+      packageRoot: __dirname,
 
-        // where to look for configuration
-        configDir: './greenlock.d',
+      // contact for security and critical bug notices
+      maintainerEmail: "anjwoc@gmail.com",
 
-        // whether or not to run at cloudscale
-        cluster: false
-    })
-    // Serves on 80 and 443
-    // Get's SSL certificates magically!
-    .serve(app);
+      // where to look for configuration
+      configDir: './greenlock.d',
+
+      // whether or not to run at cloudscale
+      cluster: false
+  })
+  // Serves on 80 and 443
+  // Get's SSL certificates magically!
+  .serve(app);
+}
+
+
